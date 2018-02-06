a344652e8cbb9b0e60290f388a3b1d882119fe15
theghman <gerardomerida08@hotmail.com>
2018-02-05 20:52:27 -0800

    Update test.cpp

--- HW1/test.cpp
+++ HW1/test.cpp
@@ -3,21 +3,20 @@
 using namespace std;
 struct charStruct
 {	//struct for charArray
-	char * charArray[20];
-	unsigned int intArray[20];
+	char ** charPointer;
+	int * intArray;
 };
 int f(int n); //Recursive function
-void initChar(charStruct &ptr);
+void initializeChar(charStruct &ptr);
 void initializeNum(charStruct &ptr);
-void accessPointer();
+void accessPointer(charStruct &ptr);
 
 int main()
 {
-	srand(time(NULL)); //Random seed
-	int optionNum;
-	charStruct charmander;//Single struct created
-	initializeNum(charmander); //Initialize number of elements for struct intArray
-	initChar(charmander);
+    charStruct charmander; //create struct
+    initializeNum(charmander);//Initialize array
+    initializeChar(charmander);
+    int optionNum;
 	do{
 		cout << "Welcome! Please choose an option:\n1) Access a pointer\n" << "2) List deallocated memory(index)\n3) Deallocate all memory\n4) Exit\n";
 		cin >> optionNum;
@@ -28,7 +27,7 @@ int main()
 		}
 		if (optionNum==1)
 			{
-				accessPointer();
+				accessPointer(charmander);
 			}
 		else if (optionNum==2)
 		{
@@ -37,7 +36,9 @@ int main()
 		else if (optionNum==3)
 		{
 			for (int i =0; i < 20;i++)
-			cout <<charmander.intArray[i] <<endl;
+			{
+			    
+			}
 		}
 		else if (optionNum==4)
 		{
@@ -53,18 +54,14 @@ return 0;
 
 void initializeNum(charStruct &ptr)
 {
-    for (int i=0;i<21;i++)
+    ptr.intArray =(int*)malloc(20*sizeof(int)); //allocate 20 block of memory
+    for (int i =0; i < 20; i ++)
     {
-        ptr.intArray[i] = f(i); //assign a value based on each index in array
-        cout << i << ") " <<ptr.intArray[i] << endl;
+        ptr.intArray[i]=f(i);//Recursive function call
     }
 }
 int f(int n)
 {
-    if (n==21)
-	{
-	    return 0;
-	}
 	//Return 2700 if zero
 	if (n == 0)
  	{
@@ -72,31 +69,37 @@ int f(int n)
 	}
 	else //Return 2 * f(n-1)
 		return (2*f(n-1));
-
 }
-void initChar(charStruct &ptr)
-{   
-    char * holdchar;
-    int SIZE;
-	for (int i = 0; i <20; i++)//Doesn't work with 19+
-	{
-	    SIZE = ptr.intArray[i];
-	    holdchar = new char[SIZE];
-	    for (int j =0; j < SIZE; j++)
-	    {
-	        holdchar[j]='A' + rand()%26;
-	    }
-		ptr.charArray[i] = holdchar;
-		delete[] holdchar;
-	}
-}
-void accessPointer()
+void initializeChar(charStruct &ptr)
 {
+   srand(time(NULL)); //Random number generation 
+   ptr.charPointer=(char**)malloc(20*sizeof(char*));//Initialize 20 char pointers
+   for (int i = 0; i < 20; i++)
+   {
+       ptr.charPointer[i]=(char*)malloc(ptr.intArray[i]*sizeof(char));//Allocate [array] number of char space
+       for(int j = 0;j<ptr.intArray[i]; j++)
+       {
+           char randomLetter = 'A' + rand()%26;
+           ptr.charPointer[i][j]=randomLetter;
+       }
+   }
+}
+void accessPointer(charStruct &ptr)
+{
+    int pointChoice;
 	int numChoice;
-	cout << "Which pointer are you trying to access? (1-20)";
-	cin >> numChoice;
+	cout << "Which pointer are you trying to access?";
+	cin >> pointChoice;
+	
 	cout << "Accessing pointer....\n";
 	cout << "1)Print first 10 chars in chosen array\n" <<"2)pDelete all chars associated with this pointer\n" <<"3)Return to main menu\n";
 	cin >> numChoice;
+	for (int i =0; i < 10;i++)
+	{
+	    cout << ptr.charPointer[numChoice][i] <<endl;
+	}
 }
+
+
+
 
